193ae57c140a2cedf7daa733001dbfe395a45b09
Dommage <dommage@hh4u.ca>
2016-10-04 20:48:35 -0400

    encode.sh
    basic

---
+++ encode.sh
@@ -0,0 +1,59 @@
+RED='\033[0;31m                ' # Red tab
+NC='\033[0m' # No Color
+quality='high' # Quality to use
+title='Omerta' # Title of the tv show "Directory name, File name"
+saison='S02' # Season of the tv show "Directory name, File name"
+resolution='720p' # Resolution of the video "Informative"
+episode=00 # One episode before the ones that you want.
+signature='ROKe' # Signature
+dir=$(printf /home/encode/dl/$title.$saison.FRENCH.$resolution.WEB-DL.H264-$signature)
+#-----------------------------
+episode=$(( episode+1 ))
+HandBrakeCLI -i "${dir}/$title.${saison}E0$episode.FRENCH.$resolution.WEB-DL.H264-$signature.flv" -t 1 --angle 1 -c 1 -o "/home/encode/finished/$title.${saison}E0$episode.FRENCH.$resolution.WEBRiP.x265-$signature.mkv"  -f mkv  -w 968 --crop 0:6:160:160 --strict-anamorphic  --modulus 2 -e x265 -q 23 --vfr -a 1 -E copy:aac -6 none -R Auto -B 0 -D 0 --gain 0 --audio-fallback ac3 --verbose=1 &&
+#-----------------------------
+episode=$(( episode+1 ))
+HandBrakeCLI -i "${dir}/$title.${saison}E0$episode.FRENCH.$resolution.WEB-DL.H264-$signature.flv" -t 1 --angle 1 -c 1 -o "/home/encode/finished/$title.${saison}E0$episode.FRENCH.$resolution.WEBRiP.x265-$signature.mkv"  -f mkv  -w 968 --crop 0:6:160:160 --strict-anamorphic  --modulus 2 -e x265 -q 23 --vfr -a 1 -E copy:aac -6 none -R Auto -B 0 -D 0 --gain 0 --audio-fallback ac3 --verbose=1 &&
+#-----------------------------
+episode=$(( episode+1 ))
+HandBrakeCLI -i "${dir}/$title.${saison}E0$episode.FRENCH.$resolution.WEB-DL.H264-$signature.flv" -t 1 --angle 1 -c 1 -o "/home/encode/finished/$title.${saison}E0$episode.FRENCH.$resolution.WEBRiP.x265-$signature.mkv"  -f mkv  -w 968 --crop 0:6:160:160 --strict-anamorphic  --modulus 2 -e x265 -q 23 --vfr -a 1 -E copy:aac -6 none -R Auto -B 0 -D 0 --gain 0 --audio-fallback ac3 --verbose=1 &&
+#-----------------------------
+episode=$(( episode+1 ))
+HandBrakeCLI -i "${dir}/$title.${saison}E0$episode.FRENCH.$resolution.WEB-DL.H264-$signature.flv" -t 1 --angle 1 -c 1 -o "/home/encode/finished/$title.${saison}E0$episode.FRENCH.$resolution.WEBRiP.x265-$signature.mkv"  -f mkv  -w 968 --crop 0:6:160:160 --strict-anamorphic  --modulus 2 -e x265 -q 23 --vfr -a 1 -E copy:aac -6 none -R Auto -B 0 -D 0 --gain 0 --audio-fallback ac3 --verbose=1 &&
+#-----------------------------
+episode=$(( episode+1 ))
+HandBrakeCLI -i "${dir}/$title.${saison}E0$episode.FRENCH.$resolution.WEB-DL.H264-$signature.flv" -t 1 --angle 1 -c 1 -o "/home/encode/finished/$title.${saison}E0$episode.FRENCH.$resolution.WEBRiP.x265-$signature.mkv"  -f mkv  -w 968 --crop 0:6:160:160 --strict-anamorphic  --modulus 2 -e x265 -q 23 --vfr -a 1 -E copy:aac -6 none -R Auto -B 0 -D 0 --gain 0 --audio-fallback ac3 --verbose=1 &&
+#-----------------------------
+episode=$(( episode+1 ))
+HandBrakeCLI -i "${dir}/$title.${saison}E0$episode.FRENCH.$resolution.WEB-DL.H264-$signature.flv" -t 1 --angle 1 -c 1 -o "/home/encode/finished/$title.${saison}E0$episode.FRENCH.$resolution.WEBRiP.x265-$signature.mkv"  -f mkv  -w 968 --crop 0:6:160:160 --strict-anamorphic  --modulus 2 -e x265 -q 23 --vfr -a 1 -E copy:aac -6 none -R Auto -B 0 -D 0 --gain 0 --audio-fallback ac3 --verbose=1 &&
+#-----------------------------
+episode=$(( episode+1 ))
+HandBrakeCLI -i "${dir}/$title.${saison}E0$episode.FRENCH.$resolution.WEB-DL.H264-$signature.flv" -t 1 --angle 1 -c 1 -o "/home/encode/finished/$title.${saison}E0$episode.FRENCH.$resolution.WEBRiP.x265-$signature.mkv"  -f mkv  -w 968 --crop 0:6:160:160 --strict-anamorphic  --modulus 2 -e x265 -q 23 --vfr -a 1 -E copy:aac -6 none -R Auto -B 0 -D 0 --gain 0 --audio-fallback ac3 --verbose=1 &&
+#-----------------------------
+episode=$(( episode+1 ))
+HandBrakeCLI -i "${dir}/$title.${saison}E0$episode.FRENCH.$resolution.WEB-DL.H264-$signature.flv" -t 1 --angle 1 -c 1 -o "/home/encode/finished/$title.${saison}E0$episode.FRENCH.$resolution.WEBRiP.x265-$signature.mkv"  -f mkv  -w 968 --crop 0:6:160:160 --strict-anamorphic  --modulus 2 -e x265 -q 23 --vfr -a 1 -E copy:aac -6 none -R Auto -B 0 -D 0 --gain 0 --audio-fallback ac3 --verbose=1 &&
+#-----------------------------
+episode=$(( episode+1 ))
+HandBrakeCLI -i "${dir}/$title.${saison}E0$episode.FRENCH.$resolution.WEB-DL.H264-$signature.flv" -t 1 --angle 1 -c 1 -o "/home/encode/finished/$title.${saison}E0$episode.FRENCH.$resolution.WEBRiP.x265-$signature.mkv"  -f mkv  -w 968 --crop 0:6:160:160 --strict-anamorphic  --modulus 2 -e x265 -q 23 --vfr -a 1 -E copy:aac -6 none -R Auto -B 0 -D 0 --gain 0 --audio-fallback ac3 --verbose=1 &&
+#-----------------------------
+episode=$(( episode+1 ))
+HandBrakeCLI -i "${dir}/$title.${saison}E$episode.FRENCH.$resolution.WEB-DL.H264-$signature.flv" -t 1 --angle 1 -c 1 -o "/home/encode/finished/$title.${saison}E$episode.FRENCH.$resolution.WEBRiP.x265-$signature.mkv"  -f mkv  -w 968 --crop 0:6:160:160 --strict-anamorphic  --modulus 2 -e x265 -q 23 --vfr -a 1 -E copy:aac -6 none -R Auto -B 0 -D 0 --gain 0 --audio-fallback ac3 --verbose=1 &&
+#-----------------------------
+episode=$(( episode+1 ))
+HandBrakeCLI -i "${dir}/$title.${saison}E$episode.FRENCH.$resolution.WEB-DL.H264-$signature.flv" -t 1 --angle 1 -c 1 -o "/home/encode/finished/$title.${saison}E$episode.FRENCH.$resolution.WEBRiP.x265-$signature.mkv"  -f mkv  -w 968 --crop 0:6:160:160 --strict-anamorphic  --modulus 2 -e x265 -q 23 --vfr -a 1 -E copy:aac -6 none -R Auto -B 0 -D 0 --gain 0 --audio-fallback ac3 --verbose=1 &&
+#-----------------------------
+episode=$(( episode+1 ))
+HandBrakeCLI -i "${dir}/$title.${saison}E$episode.FRENCH.$resolution.WEB-DL.H264-$signature.flv" -t 1 --angle 1 -c 1 -o "/home/encode/finished/$title.${saison}E$episode.FRENCH.$resolution.WEBRiP.x265-$signature.mkv"  -f mkv  -w 968 --crop 0:6:160:160 --strict-anamorphic  --modulus 2 -e x265 -q 23 --vfr -a 1 -E copy:aac -6 none -R Auto -B 0 -D 0 --gain 0 --audio-fallback ac3 --verbose=1 &&
+#-----------------------------
+episode=$(( episode+1 ))
+HandBrakeCLI -i "${dir}/$title.${saison}E$episode.FRENCH.$resolution.WEB-DL.H264-$signature.flv" -t 1 --angle 1 -c 1 -o "/home/encode/finished/$title.${saison}E$episode.FRENCH.$resolution.WEBRiP.x265-$signature.mkv"  -f mkv  -w 968 --crop 0:6:160:160 --strict-anamorphic  --modulus 2 -e x265 -q 23 --vfr -a 1 -E copy:aac -6 none -R Auto -B 0 -D 0 --gain 0 --audio-fallback ac3 --verbose=1 &&
+#-----------------------------
+episode=$(( episode+1 ))
+HandBrakeCLI -i "${dir}/$title.${saison}E$episode.FRENCH.$resolution.WEB-DL.H264-$signature.flv" -t 1 --angle 1 -c 1 -o "/home/encode/finished/$title.${saison}E$episode.FRENCH.$resolution.WEBRiP.x265-$signature.mkv"  -f mkv  -w 968 --crop 0:6:160:160 --strict-anamorphic  --modulus 2 -e x265 -q 23 --vfr -a 1 -E copy:aac -6 none -R Auto -B 0 -D 0 --gain 0 --audio-fallback ac3 --verbose=1 &&
+#-----------------------------
+episode=$(( episode+1 ))
+HandBrakeCLI -i "${dir}/$title.${saison}E$episode.FRENCH.$resolution.WEB-DL.H264-$signature.flv" -t 1 --angle 1 -c 1 -o "/home/encode/finished/$title.${saison}E$episode.FRENCH.$resolution.WEBRiP.x265-$signature.mkv"  -f mkv  -w 968 --crop 0:6:160:160 --strict-anamorphic  --modulus 2 -e x265 -q 23 --vfr -a 1 -E copy:aac -6 none -R Auto -B 0 -D 0 --gain 0 --audio-fallback ac3 --verbose=1 &&
+#-----------------------------
+episode=$(( episode+1 ))
+HandBrakeCLI -i "${dir}/$title.${saison}E$episode.FRENCH.$resolution.WEB-DL.H264-$signature.flv" -t 1 --angle 1 -c 1 -o "/home/encode/finished/$title.${saison}E$episode.FRENCH.$resolution.WEBRiP.x265-$signature.mkv"  -f mkv  -w 968 --crop 0:6:160:160 --strict-anamorphic  --modulus 2 -e x265 -q 23 --vfr -a 1 -E copy:aac -6 none -R Auto -B 0 -D 0 --gain 0 --audio-fallback ac3 --verbose=1 &&
+printf "${RED}$title $saison Finished${NC}\n"
+pause
