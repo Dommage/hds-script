9321c4aa29ecd98e59a2f0f9137f5d01ebc8b95d
Dommage <dommage@hh4u.ca>
2016-10-04 20:51:10 -0400

    Update title.sh
    add file title to all file in the directory

--- title.sh
+++ title.sh
@@ -4,7 +4,7 @@ for f in $FILES
 do
   f=${f::-4}
   echo "Processing $f file..."
-  # take action on each file. $f store current file name
+  # action
   mkvpropedit "$f.mkv" --edit info --set "title=$f"
   echo "$f"
 done
